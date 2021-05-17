bd049e370d30c3e273e0037e77560f446e54fb63
Kazuhiro Sera <seratch@gmail.com>
2021-05-17 23:02:43 +0900

    Set the version to 0.1.5-SNAPSHOT

--- core/src/main/kotlin/notion/api/Metadata.kt
+++ core/src/main/kotlin/notion/api/Metadata.kt
@@ -1,7 +1,7 @@
 package notion.api
 
 object Metadata {
-  const val VERSION: String = "0.1.4"
+  const val VERSION: String = "0.1.5-SNAPSHOT"
 
   fun isLibraryMaintainerMode(): Boolean {
     val value = System.getenv("NOTION_SDK_JVM_LIBRARY_MAINTAINER_MODE")
