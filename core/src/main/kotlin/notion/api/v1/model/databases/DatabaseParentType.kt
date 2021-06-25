215e48330801ee9cc44dc014e46648ea8dbd3778
Kazuhiro Sera <seratch@gmail.com>
2021-06-25 12:20:23 +0900

    Fix #10 by adding database.parent

---
+++ core/src/main/kotlin/notion/api/v1/model/databases/DatabaseParentType.kt
@@ -0,0 +1,10 @@
+package notion.api.v1.model.databases
+
+import com.google.gson.annotations.SerializedName
+
+enum class DatabaseParentType(val value: String) {
+  @SerializedName("workspace") Workspace("workspace"),
+  @SerializedName("page_id") PageId("page_id");
+
+  override fun toString(): String = value
+}
