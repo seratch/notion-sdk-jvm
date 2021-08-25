1a9ac48f96c9bd4887dc5280927bbf36d2af4d43
Kazuhiro Sera <seratch@gmail.com>
2021-08-25 17:31:42 +0900

    Add cover / icon properties

--- core/src/main/kotlin/notion/api/v1/request/pages/CreatePageRequest.kt
+++ core/src/main/kotlin/notion/api/v1/request/pages/CreatePageRequest.kt
@@ -6,23 +6,10 @@ import notion.api.v1.model.common.Icon
 import notion.api.v1.model.pages.PageParent
 import notion.api.v1.model.pages.PageProperty
 
-data class CreatePageRequest(
+data class CreatePageRequest @JvmOverloads constructor(
     val parent: PageParent,
     val properties: Map<String, PageProperty>,
     var children: List<Block>? = null,
     val icon: Icon? = null,
     val cover: Cover? = null,
-) {
-
-  // For other JVM languages
-  constructor(
-      parent: PageParent,
-      properties: Map<String, PageProperty>
-  ) : this(parent, properties, null, null, null)
-
-  constructor(
-      parent: PageParent,
-      properties: Map<String, PageProperty>,
-      children: List<Block>? = null,
-  ) : this(parent, properties, children, null, null)
-}
+)
