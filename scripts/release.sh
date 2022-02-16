52f45199400a4b65c153c7a264302ca70f31d36f
Kazuhiro Sera <seratch@gmail.com>
2022-02-16 17:56:03 +0900

    :verison 0.4.2

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,8 @@
 dir=`dirname $0`/..
 release_version=`sed -n 's/<version>\([^\$]\..*\)<\/version>$/\1/p' < ${dir}/pom.xml`
 
+export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
+
 if [[ "${release_version}" =~ ^.+-SNAPSHOT$ ]]; then
   profile=snapshot-releases
   mvn clean \
