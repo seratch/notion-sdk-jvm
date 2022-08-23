0626e8caa71880468a73b4c67684e6c66f29ca14
Kazuhiro Sera <seratch@gmail.com>
2022-08-23 17:47:37 +0900

    Start new version dev

--- core/src/main/kotlin/notion/api/Metadata.kt
+++ core/src/main/kotlin/notion/api/Metadata.kt
@@ -1,7 +1,7 @@
 package notion.api
 
 object Metadata {
-  const val VERSION: String = "1.7.0"
+  const val VERSION: String = "1.7.1-SNAPSHOT"
 
   fun isLibraryMaintainerMode(): Boolean {
     val value = System.getenv("NOTION_SDK_JVM_LIBRARY_MAINTAINER_MODE")
