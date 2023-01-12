8716a91428005fce7fe5d22655015d6e5e800b42
Kazuhiro Sera <seratch@gmail.com>
2023-01-12 16:23:24 +0900

    Bump dependencies

--- core/src/main/kotlin/notion/api/v1/json/gson/GsonUnknownFieldDetection.kt
+++ core/src/main/kotlin/notion/api/v1/json/gson/GsonUnknownFieldDetection.kt
@@ -20,18 +20,32 @@ class GsonUnknownFieldDetection : TypeAdapterFactory {
     val delegate = gson.getDelegateAdapter(this, type)
 
     // Check if the type adapter is a reflective, cause this solution only work for reflection.
-    if (delegate is ReflectiveTypeAdapterFactory.Adapter<*>) {
+    if (delegate is ReflectiveTypeAdapterFactory.Adapter<*, *>) {
       try {
         // Get reference to the existing boundFields.
-        val f: Field = delegate.javaClass.getDeclaredField("boundFields")
-        f.isAccessible = true
-        var boundFields = f.get(delegate) as Map<*, *>
+        var adaptorClass: Class<*> = delegate.javaClass
+        var boundFieldsField: Field? = null
+        while (boundFieldsField == null && adaptorClass != Any::class.java) {
+          try {
+            boundFieldsField = adaptorClass.getDeclaredField("boundFields")
+          } catch (_ignore: NoSuchFieldException) {
+            // Since GSON v3.10, the internal class hierarchy has been changed
+            // 1) com.google.gson.internal.bind.ReflectiveTypeAdapterFactory$FieldReflectionAdapter
+            // 2) com.google.gson.internal.bind.ReflectiveTypeAdapterFactory$Adapter
+            adaptorClass = adaptorClass.superclass
+          }
+        }
+        if (boundFieldsField == null) {
+          val message = "Failed to find bound fields inside GSON"
+          throw IllegalStateException(message)
+        }
+        boundFieldsField.isAccessible = true
+        var boundFields = boundFieldsField[delegate] as Map<*, *>
         val sb = StringBuilder()
         for (key in boundFields.keys) {
           sb.append("$key, ")
         }
         val boundFieldsStr = sb.append("...").toString()
-
         // Then replace it with our implementation throwing exception if the value is null.
         boundFields =
             object : LinkedHashMap<Any?, Any?>(boundFields) {
@@ -42,7 +56,7 @@ class GsonUnknownFieldDetection : TypeAdapterFactory {
               }
             }
         // Finally, push our custom map back using reflection.
-        f.set(delegate, boundFields)
+        boundFieldsField[delegate] = boundFields
       } catch (e: Exception) {
         // Should never happen if the implementation doesn't change.
         throw IllegalStateException(e)
